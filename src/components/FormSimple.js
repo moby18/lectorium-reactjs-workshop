f4685a948746237eccce593e29c30f7fae2eb3d8
Andrii Petrash <andrii.petrash@gmail.com>
2019-12-02 11:34:19 +0200

    reactjs forms handling

---
+++ src/components/FormSimple.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Form extends React.Component{
+  constructor (props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>sdfd</div>
+    )
+  }
+}
